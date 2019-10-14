6ce4bcbde1faee9da0011196adda1d336c5a14dd
Julius de Bruijn <bruijn@prisma.io>
2019-10-14 19:34:40 +0200

    Postgres support

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ mod dbio;
 mod queryable;
 
 use dbio::DBIO;
-use queryable::{Queryable, Sqlite};
+use queryable::{Queryable, Sqlite, Postgres};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
 #[tokio::main]
 async fn main() -> crate::Result<()> {
     let sqlite = Sqlite::new()?;
-    println!("Number is {}", sqlite.select_1().await?);
+    let psql = Postgres::new().await?;
+
+    println!("Number from Sqlite: {}", sqlite.select_1().await?);
+    println!("Number from Postgres: {}", psql.select_1().await?);
 
     Ok(())
 }
--- src/queryable.rs
+++ src/queryable.rs
@@ -1,9 +1,10 @@
 use super::DBIO;
-use futures::future;
+use futures::future::FutureExt;
 use async_std::sync::Mutex;
+use tokio_postgres::{NoTls, Config};
 
 pub trait Queryable {
-    fn select_1(&self) -> DBIO<'_, u32>;
+    fn select_1(&self) -> DBIO<u32>;
 }
 
 pub struct Sqlite {
@@ -17,7 +18,7 @@ impl Sqlite {
 }
 
 impl Queryable for Sqlite {
-    fn select_1(&self) -> DBIO<'_, u32> {
+    fn select_1(&self) -> DBIO<u32> {
         DBIO::new(async move {
             let client = self.client.lock().await;
             let mut stmt = client.prepare_cached("SELECT 1")?;
@@ -25,6 +26,42 @@ impl Queryable for Sqlite {
             let row = rows.next()?.unwrap();
 
             Ok(row.get(0)?)
+        })
+    }
+}
+
+pub struct Postgres {
+    client: Mutex<tokio_postgres::Client>
+}
+
+impl Postgres {
+    pub async fn new() -> crate::Result<Self> {
+        let (client, conn) = Config::new()
+            .user("postgres")
+            .password("prisma")
+            .host("localhost")
+            .connect(NoTls)
+            .await?;
+
+        let connection = conn.map(|r| r.unwrap());
+
+        tokio::spawn(connection);
+
+        Ok(Self { client: Mutex::new(client) })
+    }
+}
+
+impl Queryable for Postgres {
+    fn select_1(&self) -> DBIO<u32> {
+        DBIO::new(async move {
+            let client = self.client.lock().await;
+            let stmt = client.prepare("SELECT 1").await?;
+            let rows = client.query(&stmt, &[]).await?;
+
+            let row = rows.first().unwrap();
+            let result: i32 = row.get(0);
+
+            Ok(result as u32)
         })
     }
 }
